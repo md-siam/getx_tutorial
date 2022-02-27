0b23a6fc1703a83cf4801235b8959c4b4edaa300
Md. Siam <md.siam03@gmail.com>
2022-02-27 23:48:00 +0600

    getx_controller

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,4 @@
-export '/navigation/navigation.dart';
-export '/state_management/reactive/reactive_state_management.dart';
-export '/state_management/simple/simple_state_management.dart';
-export 'getx_controller/view.dart';
+export '/1_navigation/navigation.dart';
+export '/2_state_management/reactive/reactive_state_management.dart';
+export '/2_state_management/simple/simple_state_management.dart';
+export '/3_getx_controller/view.dart';
