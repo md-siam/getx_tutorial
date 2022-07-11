e61c7716d9c66626c38fd24862e92b7c3a117f78
md-siam <md.siam03@gmail.com>
2022-07-11 23:05:23 +0600

    doc: README.md file edit

--- lib/routes.dart
+++ lib/routes.dart
@@ -5,5 +5,5 @@ export '3_getx_controller/view.dart';
 export '4_dependency_management/view.dart';
 export '5_translations/translations_example.dart';
 export '6_themes/themes_example.dart';
-export '7_getx_service_&_get_view/servide_example.dart';
+export '7_getx_service_&_get_view/service_example.dart';
 export '7_getx_service_&_get_view/settings_service.dart';
